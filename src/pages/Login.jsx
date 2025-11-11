f12eae1dc8d5f6ce80a73cb8fc3f524117d3dc9a
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-11-10 21:43:27 -0300

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,16 +5,27 @@ function Login(){
         <div className='w-screen h-screen bg-fundo flex justify-between gap-35 items-center flex-col p-4 '>
       <img src={Logo} alt="" className='w-[700px] h-[250px]' />
 
-      <form action="post" className='w-[500px] h-[616px]  '>
+      <form action="post" className='w-[450px] h-[616px] flex flex-col items-center  gap-15'>
 
         <fieldset className='w-full h-[400px] border-2 border-traco flex flex-col items-center p-3 rounded-xl'>
 
           <img src="" alt="" />
-          <div className='w-[90%] h-px bg-traco'></div>
+          <div className='w-[98%] h-px bg-traco'></div>
+
           <label htmlFor="User">Usuário</label>
-          <input type="text" name='User' className='' />
+          <input type="text" name='User' className='bg-white w-[95%] h-10 rounded-[10px]' />
+
+          <label htmlFor="Password">Senha</label>
+          <input type="password" name="Password" className='bg-white w-[95%] h-10 rounded-[10px]' />
 
         </fieldset>
+
+        <button className='bg-white w-[250px] h-10 text-traco text-2xl rounded-[10px]'>
+            
+            Entrar
+
+        </button>
+
       </form>
     </div>
     )
