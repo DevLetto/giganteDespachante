813e07d1e04fd942f1358b01d3d48df46006c753
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-11-12 00:05:42 -0300

    Detalhes finais do layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,36 @@
 import Logo from '../assets/LogoGiganteDespachante.png'
+import Bolas from '../assets/bolinhas.png'
 
 function Login(){
     return(
         <div className='w-screen h-screen bg-fundo flex justify-between gap-35 items-center flex-col p-4 '>
       <img src={Logo} alt="" className='w-[700px] h-[250px]' />
 
-      <form action="post" className='w-[450px] h-[616px] flex flex-col items-center  gap-15'>
+      <form action="post" className='w-[400px] h-[616px] flex flex-col items-center  gap-15'>
 
-        <fieldset className='w-full h-[400px] border-2 border-traco flex flex-col items-center p-3 rounded-xl'>
+        <fieldset className='w-full h-[330px] border-2 border-traco flex flex-col items-center pt-5 pl-1 pr-1 rounded-xl'>
 
-          <img src="" alt="" />
-          <div className='w-[98%] h-px bg-traco'></div>
+          <div className='w-[98%] flex items-start mb-[5px]'>
+            <img src={Bolas} alt="Tres bolinhas" className='w-[85px]'/>
+          </div>
+          <div className='w-[98%] h-px bg-traco mb-[15px]'></div>
 
-          <label htmlFor="User">Usuário</label>
-          <input type="text" name='User' className='bg-white w-[95%] h-10 rounded-[10px]' />
+          <div className='w-[95%] text-start'>
+            <label htmlFor="User" className='text-2xl text-traco text-start
+            font-[arial]  '>Usuário</label>
+          </div>
+          <input type="text" name='User' className='bg-white w-[95%] h-10 rounded-[10px] mb-[50px] p-1' />
 
-          <label htmlFor="Password">Senha</label>
-          <input type="password" name="Password" className='bg-white w-[95%] h-10 rounded-[10px]' />
+
+
+          <div className='w-[95%] text-start'>
+            <label htmlFor="Password" className='text-2xl text-traco text-start font-[arial]'>Senha</label>
+          </div>
+          <input type="password" name="Password" className='bg-white w-[95%] h-10 rounded-[10px] p-1' />
 
         </fieldset>
 
-        <button className='bg-white w-[250px] h-10 text-traco text-2xl rounded-[10px]'>
+        <button className='bg-white w-[210px] h-11 text-traco text-2xl rounded-[10px] hover:bg-traco hover:text-white '>
             
             Entrar
 
