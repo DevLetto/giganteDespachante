1057417f635187e4ede095bf6bcaa3e2e92e8003
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-11-21 21:43:05 -0300

    Teste de login

--- cliente/src/pages/Login.jsx
+++ cliente/src/pages/Login.jsx
@@ -1,61 +1,100 @@
-import { useNavigate } from 'react-router-dom';
-import Logo from '../assets/LogoGiganteDespachante.png'
-import Bolas from '../assets/bolinhas.png'
+import { useNavigate } from "react-router-dom";
+import Logo from "../assets/LogoGiganteDespachante.png";
+import Bolas from "../assets/bolinhas.png";
+import { useState } from "react";
+// import axios from "axios";
 
-function Login(){
+function Login() {
+  const [user, setUser] = useState("");
+  const [senha, setSenha] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  function onSubmitClick(){
-    navigate('/menu')
+  function onSubmitClick() {
+    navigate("/menu");
   }
 
-  function handleSubmit(e){
-    e.preventDefault()
-    navigate('/menu')
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  return(
-      <div className='w-screen h-screen bg-fundo flex justify-between gap-35 items-center flex-col p-4 '>
-    <img src={Logo} alt="" className='w-[700px] h-[250px]' />
-    
+    const dadosLogin = { user, senha };
 
-    <form onSubmit={handleSubmit} action="post" className='w-[400px] h-[616px] flex flex-col items-center  gap-15'>
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dadosLogin),
+      });
 
-      <fieldset className='w-full h-[330px] border-2 border-traco flex flex-col items-center pt-5 pl-1 pr-1 rounded-lg shadow-2xl'>
+      if (response.ok) {
+        navigate("/menu");
+      } else {
+        alert("Usuario ou senha incorretos");
+      }
+    } catch (error) {
+      console.error("erro na requisição", error);
+    }
+  };
 
-        <div className='w-[98%] flex items-start mb-[5px]'>
-          <img src={Bolas} alt="Tres bolinhas" className='w-[85px]'/>
-        </div>
-        <div className='w-[98%] h-px bg-traco mb-[15px]'></div>
+  return (
+    <div className="w-screen h-screen bg-fundo flex justify-between gap-35 items-center flex-col p-4 ">
+      <img src={Logo} alt="" className="w-[700px] h-[250px]" />
 
-        <div className='w-[95%] text-start'>
-          <label htmlFor="User" className='text-2xl text-traco text-start
-          font-[arial]  '>Usuário</label>
-        </div>
-        <input type="text" name='User' className='bg-white w-[95%] h-10 rounded-[10px] mb-[50px] p-1 text-traco focus: outline-traco' required />
+      <form
+        onSubmit={handleSubmit}
+        action="post"
+        className="w-[400px] h-[616px] flex flex-col items-center  gap-15"
+      >
+        <fieldset className="w-full h-[330px] border-2 border-traco flex flex-col items-center pt-5 pl-1 pr-1 rounded-lg shadow-2xl">
+          <div className="w-[98%] flex items-start mb-[5px]">
+            <img src={Bolas} alt="Tres bolinhas" className="w-[85px]" />
+          </div>
+          <div className="w-[98%] h-px bg-traco mb-[15px]"></div>
 
+          <div className="w-[95%] text-start">
+            <label
+              htmlFor="User"
+              className="text-2xl text-traco text-start
+          font-[arial]  "
+            >
+              Usuário
+            </label>
+          </div>
+          <input
+            type="text"
+            name="User"
+            onChange={(e) => setUser(e.target.value)}
+            className="bg-white w-[95%] h-10 rounded-[10px] mb-[50px] p-1 text-traco focus: outline-traco"
+            required
+          />
 
+          <div className="w-[95%] text-start">
+            <label
+              htmlFor="Password"
+              className="text-2xl text-traco text-start font-[arial]"
+            >
+              Senha
+            </label>
+          </div>
+          <input
+            type="password"
+            name="Password"
+            onChange={(e) => setSenha(e.target.value)}
+            className="bg-white w-[95%] h-10 rounded-[10px] p-1 text-traco focus: outline-traco"
+            required
+          />
+        </fieldset>
 
-        <div className='w-[95%] text-start'>
-          <label htmlFor="Password" className='text-2xl text-traco text-start font-[arial]'>Senha</label>
-        </div>
-        <input type="password" name="Password" className='bg-white w-[95%] h-10 rounded-[10px] p-1 text-traco focus: outline-traco' required/>
-
-      </fieldset>
-
-      <button onClick={onSubmitClick} className='bg-white w-[210px] h-11 text-traco text-2xl rounded-[10px] hover:bg-traco hover:cursor-pointer hover:text-white '>
-          
+        <button
+          className="bg-white w-[210px] h-11 text-traco text-2xl rounded-[10px] hover:bg-traco hover:cursor-pointer hover:text-white "
+        >
           Entrar
-
-      </button>
-
-      
-
-    </form>
-  </div>
-  )
-
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- servidor/index.js
+++ servidor/index.js
@@ -1,31 +1,36 @@
 const express = require('express')
 const app = express()
-const bcrypt = require('bcrypt')
-const port = 8081;
+// const bcrypt = require('bcrypt')
+const fs = require('fs')
+const cors = require('cors')
+const port = 8080;
 
 app.use(express.json())
+app.use(cors())
 
-const users = []
+app.post('/login', (req, res) =>{
+    const {user, senha} = req.body
 
-app.get('/users', (req, res) =>{
-    res.json(users)
-})
+    const usersData = fs.readFileSync('./data/users.json', 'utf8')
+    const users = JSON.parse(usersData)
 
-app.post('/users', async (req, res) => {
-    try{
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.senha, salt)
-        console.log(salt)
-        console.log(hashedPassword)
-        const user = {nome: req.body.nome, senha: hashedPassword}
-        users.push(user)
-        res.status(201).send()    
-    }catch{
-        res.status(500).send()
+    const usuar = users.find(
+        (u) => u.user === user && u.senha === senha
+    )
+
+    if(!usuar){
+        return res.status(401).json({message:"usuario ou senha incoreta"})
     }
-
     
+    res.status(200).json({
+        message: "dados recebidos",
+        dadosRecebidos: {user, senha}
+    })
+
 })
+
+
+
 
 app.listen(port, ()=> console.log(`rodando na port ${port}`))
 
