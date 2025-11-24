e6dc63f900b317aadbc989b0a57cb4a369a8c098
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-11-23 23:43:20 -0300

    Tentativas fracassadas

--- servidor/src/dataBase/db.js
+++ servidor/src/dataBase/db.js
@@ -1,22 +1,48 @@
+const path = require('path')
 const Database = require('better-sqlite3')
-const db = new Database('../db.sqlite')
+const dbPath = path.join(__dirname, '../db.sqlite')
+const db = new Database(dbPath)
 
-const usuarios = `
+const dados = `
     CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         usuario TEXT NOT NULL UNIQUE,
         senha TEXT NOT NULL
     );
 
-    CREATE TABLE IF NOT EXISTS clients(
+    CREATE TABLE IF NOT EXIST clients(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL
-    )
+        nome TEXT NOT NULL,
+        cpf_cnpj TEXT NOT NULL,
+        telefone TEXT NOT NULL,
+        servico TEXT NOT NULL,
+        valor_servico TEXT NOT NULL,
+        placa TEXT NOT NULL,
+        modelo TEXT NOT NULL,
+        ano INTEGER NOT NULL,
+        chassi TEXT NOT NULL,
+        cor TEXT NOT NULL
+        data_emissao TEXT NOT NULL
+    );
+
+    CREATE INDEX IF NOT EXIST idx_cliente_servico ON clients(servico);
+
+    CREATE INDEX IF NOT EXIST idx_client_-placa ON clients(placa);
+
+    CREATE INDEX IF NOT EXIST idx_client_-data ON clients(data_emissao);
+    
+    
 `;
 
-db.exec(usuarios)
+try{
+    db.exec(dados)
+    console.log("Taela criada")
+}catch(err){
+    console.log("error", err)
+}
 
 
+module.export = db;
 
 
 
--- servidor/src/index.js
+++ servidor/src/index.js
@@ -4,30 +4,40 @@ const app = express()
 const fs = require('fs')
 const cors = require('cors')
 const port = 8080;
+const db = require('./dataBase/db')
 
 app.use(express.json())
 app.use(cors())
 
-app.post('/login', (req, res) =>{
-    const {user, senha} = req.body
+// app.post('/login', (req, res) =>{
+//     const {user, senha} = req.body
 
-    const usersData = fs.readFileSync('./data/users.json', 'utf8')
-    const users = JSON.parse(usersData)
+//     const usersData = fs.readFileSync('./data/users.json', 'utf8')
+//     const users = JSON.parse(usersData)
 
-    const usuar = users.find(
-        (u) => u.user === user && u.senha === senha
-    )
+//     const usuar = users.find(
+//         (u) => u.user === user && u.senha === senha
+//     )
 
-    if(!usuar){
-        return res.status(401).json({message:"usuario ou senha incoreta"})
-    }
+//     if(!usuar){
+//         return res.status(401).json({message:"usuario ou senha incoreta"})
+//     }
     
-    res.status(200).json({
-        message: "dados recebidos",
-        dadosRecebidos: {user, senha}
-    })
+//     res.status(200).json({
+//         message: "dados recebidos",
+//         dadosRecebidos: {user, senha}
+//     })
 
-})
+// })
+
+try{
+    const clientes = db.prepare("SELECT * FROM clients").all();
+    console.log("deu bom")
+
+}catch(err){
+    console.log('error', err)
+}
+console.log("DB: ",db)
 
 
 
